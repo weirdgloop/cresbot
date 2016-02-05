f022dc514e336e899903a3967b2021ad289f4a84
onei <mdowdell244@gmail.com>
2016-02-05 10:46:25 +0000

    Update cresbot.py

--- cresbot.py
+++ cresbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # ----------------------------------------------------------------------
-# Copyright (c) 2015 Matthew Dowdell <mdowdell244@gmail.com>.
+# Copyright (c) 2015-2016 Matthew Dowdell <mdowdell244@gmail.com>.
 # This file is part of Cresbot.
 #
 # Cresbot is free software: you can redistribute it and/or modify

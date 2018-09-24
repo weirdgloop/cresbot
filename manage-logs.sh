d9eba9b3f81c58f7bf238c10f0ee1d3c62eef3b9
onei <cqm.fwd@gmail.com>
2018-09-24 23:09:14 +0000

    fix manage logs script

--- manage-logs.sh
+++ manage-logs.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 # Compress log files to save space and remove old logs
 
-MAX=10
+rotate() {
+	local -r MAX=10
+	local i=1
 
-i=1
+	cd /home/cqm/cresbot/logs/
 
-cd /home/cqm/cresbot/logs/
+	for log_file in hiscorecounts-*; do
+		# ignore the first one in case it's still in use
+		if [ $i != 1 ]; then
+			# compress log file - no op if it's already compressed
+			gzip $log_file
 
-for log_file in $(ls -lt | grep hiscorecounts-*); do
-	# ignore the first one in case it's still in use
-	if [ $i == 1 ]; then
-		continue
-	fi
+			if [ "$i" -gt "$MAX" ]; then
+				rm -f $log_file
+			fi
+		fi
 
-	# compress log file - no op if it's already compressed
-	gzip $log_file
+		((i++))
+	done
+}
 
-	if [ $i > $max ]; then
-		rm -f $log_file
-	fi
-
-	((i++))
-done
+rotate

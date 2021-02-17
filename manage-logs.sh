e76ca7439227ad7d7f37c018c696cac6885c9213
onei <cqm.fwd@gmail.com>
2021-02-17 17:21:00 +0000

    fix log rotation

--- manage-logs.sh
+++ manage-logs.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 # Compress log files to save space and remove old logs
 
+set -x
+
 rotate() {
 	local -r MAX=40
 	local i=1
 
 	cd /home/cqm/cresbot/logs/
 
-	for log_file in $(ls -t hiscorecounts-*.log); do
+	for log_file in $(ls -t hiscorecounts-*.log*); do
 		# ignore the first one in case it's still in use
 		if [[ $i != 1 ]]; then
 			# compress log file - no op if it's already compressed
@@ -21,14 +23,16 @@ rotate() {
 		((i++))
 	done
 
-	for log_file in $(ls -t hiscorecounts-*.json); do
+        i=1
+
+	for json_file in $(ls -t hiscorecounts-*.json*); do
 		# ignore the first one in case it's still in use
 		if [[ $i != 1 ]]; then
-			# compress log file - no op if it's already compressed
-			gzip $log_file
+			# compress json file - no op if it's already compressed
+			gzip $json_file
 
 			if (( $i > $MAX )); then
-				rm -f $log_file
+				rm -f $json_file
 			fi
 		fi
 
